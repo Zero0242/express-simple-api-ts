3db2fc8b5d9dcc77a232c492197070461d4715e4
Zero0242 <91895271+Zero0242@users.noreply.github.com>
2024-01-03 09:30:39 -0300

    DB fixes

--- src/db.ts
+++ src/db.ts
@@ -3,9 +3,9 @@ import { MONGO_CNN } from "./config";
 
 export const DBConnection = async () => {
   try {
+    console.log('Conectando a base de datos---');
     await mongoose.connect(MONGO_CNN);
-    console.log('Conexion exitosa');
-    
+    console.log('Base de datos online');    
   } catch (error) {
     console.error(error);
   }

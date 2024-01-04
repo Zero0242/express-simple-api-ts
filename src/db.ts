b0f4b0d1f9c1c8725cb2487cd185e5f200aae185
Zero0242 <91895271+Zero0242@users.noreply.github.com>
2024-01-04 10:43:14 -0300

    SEED Controller

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import { MONGO_CNN } from "./config";
 
 export const DBConnection = async () => {
   try {
-    console.log('Conectando a base de datos---');
+    console.log('Conectando a base de datos...');
     await mongoose.connect(MONGO_CNN);
     console.log('Base de datos online');    
   } catch (error) {

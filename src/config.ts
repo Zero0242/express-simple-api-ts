5c9b063827f7380d15deea2b02a4ac00146aae8a
Zero0242 <pedrocorream66@gmail.com>
2024-03-28 17:11:03 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT;
-const MONGO_CNN = process.env.MONGO_CNN;
+const MONGO_CNN = process.env.MONGO_CNN??'mongodb://localhost:27017/express-db';
 
 const JWT_KEY = process.env.JWT_KEY;
 const JWT_DURATION = process.env.JWT_DURATION;
